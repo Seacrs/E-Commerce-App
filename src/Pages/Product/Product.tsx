f099d7a22d4c3a3769c6dde0b1fef14920c033ce
Seacrs <shemachris072@gmail.com>
2026-04-07 21:04:07 +0200

    feat: implement product detail page with nested routes and loader

--- src/Pages/Product/Product.tsx
+++ src/Pages/Product/Product.tsx
@@ -1,5 +1,7 @@
-import {Link, Outlet, NavLink, useLoaderData, type LoaderFunctionArgs } from 'react-router-dom'
-import { requireAuth } from '../../util'
+import {Link, Outlet, NavLink, useLoaderData, type LoaderFunctionArgs } from 'react-router-dom';
+import clsx from 'clsx';
+import { requireAuth } from '../../util';
+import { getProduct, type ProductResponse } from "../../api";
 
 export async function loader({ params }: LoaderFunctionArgs){
     await requireAuth();
@@ -8,12 +10,49 @@ export async function loader({ params }: LoaderFunctionArgs){
     if(!id){
         throw new Response("Category ID is required", { status: 400 });
     }
-    return 
+
+    return getProduct(id);
 }
 export default function Product(){
+    const data = useLoaderData<ProductResponse>();
+    const activeStyles = ({ isActive } : { isActive : boolean}) => clsx(
+        `no-underline text-gray-500 font-medium px-2 py-4 -ml-6 hover:text-gray-800 hover:underline hover:font-bold`, 
+        isActive && "font-bold underline text-gray-800")        
     return (
         <div>
-
+            <Link to=".."
+                relative="path"
+                className="decoration-0 mt-14 ml-7"
+                >&larr; 
+                <span>Back to all vans</span>
+            </Link>
+            {data.error && <h3 className="text-red-400 text-xl">{data.error}</h3>}
+            {!data.error && data.product && 
+                <div className='p-6 mx-7 my-6 bg-white'>
+                    <div className='flex flex-col gap-3'>
+                        <img src={data.product.images[0]} alt="" />
+                        <div>
+                            <h3 className='mt-6 mb-1 text-xl text-gray-900'>{data.product.title}</h3>
+                            <h4 className='text-lg text-gray-900'>${data.product.price}</h4>
+                        </div>
+                    </div>
+                    <nav className='flex gap-2 my-6'>
+                        <NavLink
+                            to="."
+                            className={activeStyles}
+                        >
+                            Details
+                        </NavLink>
+                        <NavLink
+                            to="photos"
+                            className={activeStyles}
+                        >
+                            Photos
+                        </NavLink>
+                    </nav>
+                    <Outlet context={data.product}/>
+                </div>
+            }
         </div>
     )
 }
